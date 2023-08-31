3a52ff3801628612288286996bbc0853f89eb59d
themnsavage <mnsavage@crimson.ua.edu>
2023-08-31 12:06:30 -0500

    feat: allow get protocol to retrieve item data from table

--- src/get.py
+++ src/get.py
@@ -1,9 +1,20 @@
 import json
+import boto3
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
+    primary_key = str(event.get('pathParameters', {}).get('id'))
+    
+    response = table.get_item(
+        Key={'id': primary_key}
+    )
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello from GET method",
+            "data": response.get('Item',{}).get('data'),
         }),
     }
