eb4c55d6cec3a67f2b2e12f26d693202db852954
themnsavage <mnsavage@crimson.ua.edu>
2023-08-30 22:06:15 -0500

    refactor: remove extra code and add comments

--- src/post.py
+++ src/post.py
@@ -6,7 +6,6 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
-    print(event)
     body = json.loads(event["body"])
     item = {
         'id': body['id'],
@@ -14,7 +13,6 @@ def lambda_handler(event, context):
     }
 
     response = table.put_item(Item=item)
-    print('made pass put item')
     
     return {
         "statusCode": 200,
