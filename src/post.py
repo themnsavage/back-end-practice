230d568f5263092fe9cb1f850f390d57ed0c72a3
themnsavage <mnsavage@crimson.ua.edu>
2023-08-30 21:59:44 -0500

    feat: add dynamodb and post protocol to table

--- src/post.py
+++ src/post.py
@@ -1,9 +1,24 @@
 import json
+import boto3
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
+    print(event)
+    body = json.loads(event["body"])
+    item = {
+        'id': body['id'],
+        'data': body['data']
+    }
+
+    response = table.put_item(Item=item)
+    print('made pass put item')
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello from POST method",
+            "message": "Item was added",
         }),
     }
